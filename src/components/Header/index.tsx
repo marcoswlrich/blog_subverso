a2bb04ac2747c22842dad1453cfce88a1880f794
marcoswlrich <marcosapw.dev@gmail.com>
2022-08-16 22:52:59 -0400

    Config 2

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+export function Header() {
+  return (
+    <header>
+      <div>
+        <nav>
+          <ul>
+            <li>Home</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import Head from 'next/head';
 
-import styles from  '../styles/home.module.scss'
-
 export default function Home() {
   return (
     <>
       <Head>
       <title>Subverso</title>
     </Head>
-      <h1 className={ styles.title}>
+      <h1>
       
         Hello Word
       </h1>
