f3f0f2a49e6b137a3bafb1401a9a1facef20d7fa
marcoswlrich <marcosapw.dev@gmail.com>
2022-08-19 11:28:24 -0400

    Navbar responsive

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,21 +6,22 @@ import { AiOutlineClose } from "react-icons/ai";
 import styles from './styles.module.scss';
 
 export function Header() {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const [size, setSize] = useState({
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+  const [size, setSize] = useState<any>({
     width: 0,
-    height:0,
+    height: 0,
   });
 
   useEffect(() => {
-    const handleResize = () => {
+      const handleResize = () => {
       setSize({
         width: window.innerWidth,
         height: window.innerHeight,
       });
-      window.addEventListener('resize', handleResize);
+    
+    window.addEventListener('resize', handleResize);
 
-      return () => window.removeEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
     }
   }, []);
 
@@ -39,11 +40,11 @@ export function Header() {
       <div className={styles.header__content}>
          <h2 className={styles.header__content__logo}>Subverso</h2>
       
-        <nav className={ `{styles.header__content__nav} ${
+        <nav className={ `${styles.header__content__nav} ${
           menuOpen && size.width < 768 ? styles.isMenu : ""
         }`} 
         >
-          <ul className={styles.header__content__nav__list}>
+          <ul className={styles.menuList}>
             <li className={styles.list}><a href="#" className={styles.link} onClick={menuToggleHandler}>Home</a></li>
             <li className={styles.list}><a href="" className={styles.link} onClick={menuToggleHandler}>Categorias</a></li>
             <li className={styles.list}><a href="" className={styles.link} onClick={menuToggleHandler}>Autor</a></li>
