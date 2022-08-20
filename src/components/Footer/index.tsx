a359e015491b4ea9de9c2a89d0cda5fa1b8b7828
marcoswlrich <marcosapw.dev@gmail.com>
2022-08-20 02:29:40 -0400

    Footer pag init

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.scss';
+
+export function Footer() {
+  return (
+    <footer>
+      <div className={styles.container} >
+        <h2>Subverso</h2>
+      </div>
+    </footer>
+  )
+}
