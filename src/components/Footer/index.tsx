7181faecb6ff0bf3525ff7b60a1ffbbf06c433cd
marcoswlrich <marcosapw.dev@gmail.com>
2022-08-30 01:28:57 -0400

    Footer styles

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,36 @@
+import { linkSync } from "fs";
+import { FaInstagram, FaTwitter, FaFacebook } from "react-icons/fa";
+
 import styles from './styles.module.scss';
+
+const footerNav = [
+  {
+    id: 1,
+    headline: "Recursos gerais",
+    links: ["Blog", "Categorias", "Autor"],
+  },
+  {
+    id: 2,
+    headline: "Categorias",
+    links: ["Musica", "Literatura", "Autoral"],
+  },
+  {
+    id: 3,
+    headline: "Subverso",
+    links: ["Contato", "Sobre", "Política de privacidade"],
+  },
+  // {
+  //   id: 3,
+  //   headline: "Redes sociais",
+  //   links: ["Twitter", "Instagram", "Facebook"]
+  // },
+];
+
+// const socials = [
+//   <FaInstagram />,
+//   <FaTwitter />,
+//   <FaFacebook />
+// ]
 
 export function Footer() {
   return (
@@ -15,37 +47,29 @@ export function Footer() {
           </div>
         </div>
         <div className={styles.footer__content} >
-          <div className={styles.footer__content__col}>
-            <div className={styles.footer__content__col__headline}></div>
+          {footerNav.map(({id, headline, links}) => (
+          <div className={styles.footer__content__col} key={id}>
+            <div className={styles.footer__content__col__headline}>{headline}</div>
             <ul className={styles.footer__content__col__links}>
-              <li>
-                <a href="">opcao 1</a>
+              {links.map((link, index) => (
+              <li key={index + 1 }>
+                <a href="">{link}</a>
               </li>
-              <li>
-                <a href="">opcao 2</a>
-              </li>
-              <li>
-                <a href="">opcao 3</a>
-              </li>
-              <li>
-                <a href="">opcao 4</a>
-              </li>
-              <li>
-                <a href="">opcao 5</a>
-              </li>
+              ))}
             </ul>
           </div>
-          
+          ))}
           
         </div>
         <div className={styles.footer__base}>
-          <h2>Subverso</h2>
-          <span className={styles.footer__base__year}>
-          Subverso&nbsp;&copy;&nbsp;{new Date().getFullYear()} - Desenvolvido por Marcos Wlrich
-          </span>
+          {/* <h2 className={styles.footer__base__logo}>Subverso</h2> */}
+          <p className={styles.footer__base__year}>
+            Subverso © {new Date().getFullYear()}. Todos os direitos reservados.
+          </p>
           <ul className={styles.footer__base__socials}>
-            <li><a className={styles.sociais} href="">teste</a></li>
-            <li><a href="">teste</a></li>
+            <li><a className={styles.sociais} href=""><FaTwitter /></a></li>
+            <li><a className={styles.sociais} href=""><FaInstagram /></a></li>
+            <li><a className={styles.sociais} href=""><FaFacebook /></a></li>
           </ul>
         </div>
       
